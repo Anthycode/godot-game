b0aec05b60a3043d8b305fa1f913427a6fce52b7
Anthycode <133506955+Anthycode@users.noreply.github.com>
2023-05-15 10:01:28 +0200

    nouveau

--- Bullets/bullet.gd
+++ Bullets/bullet.gd
@@ -2,7 +2,5 @@ extends Area2D
 
 @export var speed: float = 500
 func _physics_process(delta):
-	global_position.y -= speed * delta 
-	
-	
+	position.y -= speed * delta 
 	
--- Mainplayer/Sprite2D.gd
+++ Mainplayer/Sprite2D.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var plBullet := preload("res://Bullets/bullet.gd")
 
